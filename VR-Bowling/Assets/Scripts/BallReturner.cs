4304a85bd15bc05e5acbd0d732478c32d24fa5c2
KellyVKT <157765636+KellyVKT@users.noreply.github.com>
2024-11-12 11:33:16 -0600

    Updated BallReturner Script

--- VR-Bowling/Assets/Scripts/BallReturner.cs
+++ VR-Bowling/Assets/Scripts/BallReturner.cs
@@ -4,6 +4,7 @@
  */
 using System.Collections;
 using System.Collections.Generic;
+using Unity.XR.CoreUtils;
 using UnityEngine;
 
 public class BallReturner : MonoBehaviour
@@ -14,18 +15,77 @@ public class BallReturner : MonoBehaviour
     public List<GameObject> ballsOnReturner; // The # of balls on returner
     public int spawnLimit; // Max # of balls can spawn
 
-    public List<Vector2> uvOffset; // List of offset for ball texture
+    public List<float> uvOffset; // Offset Y for ball texture
+
+    // For the balls to roll forawrd
+    public float moveSpeed = 2f;
+    public float rotateSpeed = 2f;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        // 0 - 3
+        uvOffset.Add(0.0f);
+        uvOffset.Add(0.2482f);
+        uvOffset.Add(0.2482f*2);
+        uvOffset.Add(0.2482f*3);
+    }
+
+    // Call this to return the ball when it drops end of lane
+    public void ReturnBall()
+    {
+
+    }
+
+    // Instantiates and Randomizes ball texture
+    private void RandomizeUV(GameObject ball)
+    {
+
+        Renderer ballRenderer = ball.GetComponent<Renderer>();
+
+        if (ballRenderer != null)
+        {
+            Material ballMaterial = ballRenderer.material;
+
+            // Generate a random UV offset
+
+            float randomOffsetY = Random.Range(0, uvOffset.Count);
+
+            // Now apply it to the ball texture
+            // Apply the random offset to the material's main texture
+            ballMaterial.SetTextureOffset("_MainTex", new Vector2(0f, randomOffsetY));
+
+            // If you have additional maps, apply the same offset
+            // ballMaterial.SetTextureOffset("_BumpMap", new Vector2(randomOffsetX, randomOffsetY));
+
+        }
+
+    }
+
+    // Coroutine to move and rotate the balls forward
+    //private IEnumerator MoveAndRotateBallsForward()
+    
+
+    // Trigger detection for when ball fall through lane
+    private void OnTriggerEnter(Collider other)
+    {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    // Function to create a new ball
+    public void CreateNewBall()
     {
-        
+        // Instantiate a new ball at the spawn point
+        GameObject newBall = Instantiate(ballPrefab, spawnPoint.position, Quaternion.identity);
+
+        // Randomize its Y UV texture offset
+        RandomizeUV(newBall);
+
+        // Add the new ball to the returner queue
+        ballsOnReturner.Add(newBall);
+
+        // Start moving and rotating the balls forward
+        //StartCoroutine(MoveAndRotateBallsForward());
     }
 }
