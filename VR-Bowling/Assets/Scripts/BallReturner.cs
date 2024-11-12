9ae085b6401fcd38b9f9b9179bc786fd356bb1d2
KellyVKT <157765636+KellyVKT@users.noreply.github.com>
2024-11-12 11:05:07 -0600

    Updated BallReturner Script

--- VR-Bowling/Assets/Scripts/BallReturner.cs
+++ VR-Bowling/Assets/Scripts/BallReturner.cs
@@ -1,9 +1,22 @@
+/* 
+ * This script instantiates a set number of bowling balls with randomized UV coordinates.
+ * Also handles when ball "drops" from end of lane and returns it to the ball returner.
+ */
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BallReturner : MonoBehaviour
 {
+    public Transform spawnPoint; // Where ball is instanced/returned. Might change depending on lane played.
+    
+    public GameObject ballPrefab;
+    public List<GameObject> ballsOnReturner; // The # of balls on returner
+    public int spawnLimit; // Max # of balls can spawn
+
+    public List<Vector2> uvOffset; // List of offset for ball texture
+
+
     // Start is called before the first frame update
     void Start()
     {
